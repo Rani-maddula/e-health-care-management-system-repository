d94c73c2e6906022d9c36aaccaa282ef17f112fb
M Rani <M Rani@192.168.11.31>
2019-10-30 14:48:59 +0530

    First Commit By Rani

---
+++ e-health-care-management-system/src/main/java/com/iiht/agnostic/hyd1/ehealth/care/management/system/EHealthCareManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.iiht.agnostic.hyd1.ehealth.care.management.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EHealthCareManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EHealthCareManagementSystemApplication.class, args);
+	}
+
+}
---
+++ e-health-care-management-system/src/test/java/com/iiht/agnostic/hyd1/ehealth/care/management/system/EHealthCareManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iiht.agnostic.hyd1.ehealth.care.management.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EHealthCareManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
